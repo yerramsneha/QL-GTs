3dd929250ccbe4d1187ff6c6a70b3038407ceecb
Sai Nitin Bogavarapu <57945006+nitin264@users.noreply.github.com>
2022-07-06 17:11:16 +0530

    Add files via upload

---
+++ clickfun.js
@@ -0,0 +1,3 @@
+function clickfun() {
+    document.alert("Submitted successfully");
+}
---
+++ script.js
@@ -0,0 +1,10 @@
+function message() {
+    window.alert("Submitted successfully !");
+}
+
+const d = new Date;
+d.getDate()
+
+function myFun() {
+    document.getElementById("demoBut").innerHTML = "Submitted at " + d;
+}
